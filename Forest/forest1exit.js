c627ac196d8607b1a35e04c3485d6437c33086ef
LampikoskiO <onni.lampikoski@gmail.com>
2020-11-20 15:13:00 +0200

    blabla

--- Forest/forest1exit.js
+++
@@ -1,8 +0,0 @@
-function forest1exit() {
-    document.getElementById("text").textContent = "You clicked option 4, you exit the forest." + "</br>" + "Please wait a few seconds :)";
-    setTimeout('', 3000);
-    forest1();
-    console.log('foreast1exit');
-}
-
-//not working?? SetTimeout not working ugggghhh
